2c3b4af0ac66085407cc991826ef7f6afab4d704
beewp <ganghyoc@gmail.com>
2022-02-21 21:49:54 +0900

    test 용 get users

--- routes/login.js
+++ routes/login.js
@@ -7,25 +7,31 @@ dotenv.config();
 
 const router = express.Router();
 
-router.post('/',(async (req, res) => {
-        const { user_id, user_pw } = req.body;
+router.get('/', async (req, res) => {
+    const users = await User.findAll()
 
-        console.log( user_pw, user_id );
-        const user = await User.findOne({
-            where: {
-                userId : user_id,
-            }
-        });
-        if (!user || user_pw !== user.password) {
-            return res.status(400).send({
-                errorMessage: "아이디 또는 패스워드가 잘못됐습니다."
-            });
+    res.json(users);
+});
+
+router.post('/', (async (req, res) => {
+    const { user_id, user_pw } = req.body;
+
+    console.log(user_pw, user_id);
+    const user = await User.findOne({
+        where: {
+            userId: user_id,
         }
+    });
+    if (!user || user_pw !== user.password) {
+        return res.status(400).send({
+            errorMessage: "아이디 또는 패스워드가 잘못됐습니다."
+        });
+    }
 
-        const mytoken = jwt.sign({ user_id: user.userId }, process.env.JWT_KEY);
-        console.log(mytoken);
-        res.send({mytoken});
-    })
+    const mytoken = jwt.sign({ user_id: user.userId }, process.env.JWT_KEY);
+    console.log(mytoken);
+    res.send({ mytoken });
+})
 );
 
 module.exports = router;
