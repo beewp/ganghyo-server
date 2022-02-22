d923ec3a86ed639c43400088dc1aea3b911df426
beewp <ganghyoc@gmail.com>
2022-02-22 19:46:26 +0900

    [Fix] add fs multer aws-sdk

--- routes/board.js
+++ routes/board.js
@@ -1,10 +1,41 @@
 const express = require("express");
+const multer = require("multer");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+
+dotenv.config();
 const AuthMiddlewares = require("../middlewares/AuthMiddlewares");
-const { Board, Comment, Like } = require("../models")
-const { Op } = require("sequelize")
+const { Board, Comment, Like } = require("../models");
+const { Op } = require("sequelize");
 const router = express.Router();
 
+try{
+    fs.readdirSync("uploads");
+} catch(error){
+    console.error("uploads 폴더가 없어 생성합니다.");
+    fs.mkdirSync("uploads");
+}
 
+AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2',
+});
+
+const uploads = multer({
+    storage: multerS3({
+        s3: new AWS.S3(),
+        bucket: 'pracimgmanage',
+        key(req, file, cb){
+            cb(null, `original/${Date.now()}`);
+        },
+    }),
+});
+
+router.post('/img', upload.single("img"))
 // /post
 router.route('/')
 //전체 게시물 찾기
@@ -51,24 +82,24 @@ router.route('/:postId')
 });
 //   /post/:postId/like
 router.get('/:postId/like',async(req, res) => {
-    const { postId } = req.params
+    const { postId } = req.params;
 
-    const likes = await Board.findAll({ include: [{
+    const post = await Board.findAll({ include: [{
         model: Like,
         where: {[Op.and]: [{postId}, {check: true}]},
     }]});
 
-    return res.json(likes);
+    return res.json(post.likes);
 });
 
 router.post('/:postId/like',async (req, res) => {
     const { postId } = req.params;
-    const userId = 'test1';
+    const { userId }= req.body;
     const check = true;
     console.log(postId, userId, check);
-    // const likes = await Like.create( {postId, userId, check} );
+    const likes = await Like.create( {postId, userId, check} );
 
-    return res.json(userId);
+    return res.json({ success: true });
 });
 
 
