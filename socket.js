28f491147fee54b3bf49af440cc13eeb9d8d909b
beewp <ganghyoc@gmail.com>
2022-02-22 19:45:00 +0900

    [Fix] ing...

--- socket.js
+++ socket.js
@@ -1,20 +1,20 @@
 const SocketIO = require('socket.io');
-module.exports = (server, app, sessionMiddleware) => {
-  const io = SocketIO(server, { path: '/socket.io', cors:{
-      origin:"*",
-      methods:["GET","POST"],
-  } } );
+module.exports = (server, app) => {
+  const io = SocketIO(server, {
+    path: '/socket.io', cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    }
+  });
   app.set('io', io);
 
   io.use((socket, next) => {
-    // sessionMiddleware(socket.request, socket.request.res || {}, next);
+
   });
 
   io.on('connection', (socket) => { // 웹소켓 연결 시
     const req = socket.request;
-    console.log('새로운 클라이언트 접속!', ip, socket.id);
-
-    io.to()
+    console.log('새로운 클라이언트 접속!', socket.id);
 
     socket.on('disconnect', () => { // 연결 종료 시
       console.log('클라이언트 접속 해제', ip, socket.id);
