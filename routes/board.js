6f10a05388872c35a1be3185b4062bf8a58d514e
beewp <ganghyoc@gmail.com>
2022-02-23 13:24:18 +0900

    [Fix] add put like

--- routes/board.js
+++ routes/board.js
@@ -8,16 +8,9 @@ const AWS = require("aws-sdk");
 
 dotenv.config();
 const AuthMiddlewares = require("../middlewares/AuthMiddlewares");
-const { Board, Comment, Like } = require("../models");
+const { Board, Like } = require("../models");
 const { Op } = require("sequelize");
 const router = express.Router();
-
-try{
-    fs.readdirSync("uploads");
-} catch(error){
-    console.error("uploads 폴더가 없어 생성합니다.");
-    fs.mkdirSync("uploads");
-}
 
 AWS.config.update({
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
@@ -25,79 +18,95 @@ AWS.config.update({
     region: 'ap-northeast-2',
 });
 
-const uploads = multer({
+const upload = multer({
     storage: multerS3({
         s3: new AWS.S3(),
         bucket: 'pracimgmanage',
-        key(req, file, cb){
+        key(req, file, cb) {
             cb(null, `original/${Date.now()}`);
         },
     }),
 });
 
-router.post('/img', upload.single("img"))
+
 // /post
-router.route('/')
-//전체 게시물 찾기
-    .get(async (req, res) =>{
-        const posts = await Board.findAll({
-            order: [["createdAt", "DESC"]],
-        });
+router.get('/', async (req, res) => {
+    const posts = await Board.findAll({
+        order: [["createdAt", "DESC"]],
+    });
 
-        res.json({ posts });
-    })
-    .post( async (req, res)=>{
-        const { user_id, post_img, post_content } = req.body;
-
-        await Board.create({ userId: user_id, img: post_img, content: post_content});
-
-        res.json({success : true });
+    res.json({ posts });
 });
+router.post('/', AuthMiddlewares, upload.single('img'), async (req, res) => {
+    const { user_id, post_content } = req.body;
+    const img = req.file.location;
+    await Board.create({ userId: user_id, img, content: post_content });
+
+    res.json({ success: true });
+});
+
 //   /post/:postId
 router.route('/:postId')
-    .get(async (req, res) =>{
+    .get(async (req, res) => {
         const { postId } = req.params;
         const post = await Board.findByPk(postId);
 
         res.json({ post });
     })
-    .delete( async (req, res) => {
+    .delete(AuthMiddlewares, async (req, res) => {
         const { postId } = req.params;
 
+        await Board.destroy({ where: { postId } })
+            .then(count => {
+                if (!count) {
+                    return res.status(404).send({ error: '삭제할 데이터가 없습니다.' });
+                }
+            });
+        res.json({ success: true });
+    });
 
-        await Board.destroy({where: { postId }})
-        .then(count => {
-            if (!count){
-                return res.status(404).send({error: '삭제할 데이터가 없습니다.'});
-            }
-        });
-        res.json({ success: true });
-    })
-    .put( async (req, res) => {
-        const { postId } = req.params;
-        const { post_img, post_content } = req.body;
-        await Board.update({ img : post_img, content: post_content },
-             {where:{ postId, }});
-        res.json({ success: true });
+router.put(AuthMiddlewares, upload.single('img'), async (req, res) => {
+    const { postId } = req.params;
+    const { post_content } = req.body;
+    const img = req.file.location;
+    await Board.update({ img, content: post_content },
+        { where: { postId } });
+    res.json({ success: true });
 });
 //   /post/:postId/like
-router.get('/:postId/like',async(req, res) => {
+router.get('/:postId/like', async (req, res) => {
     const { postId } = req.params;
 
-    const post = await Board.findAll({ include: [{
-        model: Like,
-        where: {[Op.and]: [{postId}, {check: true}]},
-    }]});
+    const post = await Board.findAll({
+        include: [{
+            model: Like,
+            where: { [Op.and]: [{ postId }, { check: true }] },
+        }]
+    });
 
     return res.json(post.likes);
 });
 
-router.post('/:postId/like',async (req, res) => {
+router.put('/:postId/like', async (req, res) => {
     const { postId } = req.params;
-    const { userId }= req.body;
-    const check = true;
-    console.log(postId, userId, check);
-    const likes = await Like.create( {postId, userId, check} );
+    const { user_id } = req.body;
+
+    const existLike = await Like.find({
+        where: {
+            [Op.and]: [{ postId }, { userId: user_id }],
+        },
+    });
+    if (!existLike) {
+        const likes = await Like.create({ postId, userId, check: true });
+    } else {
+        await Like.destroy({
+            where: {
+                [Op.and]: [{ postId }, { userId: user_id }],
+            },
+        });
+    }
+    // 이러면 3번 db를 이용하기 때문에 성능에 문제가 있지 않을까?
+
 
     return res.json({ success: true });
 });
