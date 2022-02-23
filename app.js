09a59bbf03e6b94a71e69bf6edf535bf55197b27
beewp <ganghyoc@gmail.com>
2022-02-23 19:11:47 +0900

    [Fix] cors

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ dotenv.config();
 
 const app = express();
 
-const cors = require("cors");
+// const cors = require("cors");
 
-whitelis =[];
+// whitelis =[];
 
-const corsOptions = {
-    origin: '*', //이부분에 사이트 url이 필요
-    Credentials: true
-}
+// const corsOptions = {
+//     origin: '*', //이부분에 사이트 url이 필요
+//     Credentials: true
+// }
 
 const SocketIO = require('./socket');
 const indexRouter = require('./routes/index');
