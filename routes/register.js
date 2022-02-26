40f4e356783e2f9f73cfb079b81dc2141c77d6d2
beewp <ganghyoc@gmail.com>
2022-02-26 16:20:45 +0900

    [Fix] divide router, controller

--- routes/register.js
+++ routes/register.js
@@ -1,31 +1,14 @@
 const express = require("express");
-const { User } = require("../models");
-const { Op } = require("sequelize");
+const { postRegister } = require("../controllers/register");
 const { checkRegister } = require("../middlewares/CheckInputMiddlewares");
 const { checkNotLogin } = require("../middlewares/CheckLoginedMiddlewares");
 
 const router = express.Router();
 
-router.get("/", checkNotLogin, async(req, res) =>{
+router.get("/", checkNotLogin, (req, res) =>{
   res.send();
 });
 
-router.post('/',checkRegister, (async (req, res) => {
-    const { user_id, nickname, user_pw } = req.body;
-    const existUsers = await User.findOne({
-      where: {
-        [Op.or]: [{ nickname }, { userId : user_id }],
-      },
-    });
-    if (existUsers) {
-      return res.status(400).send({
-        errorMessage: "가입된 아이디 또는 닉네임이 있습니다.",
-      });
-    }
-    await User.create({ userId: user_id, nickname, password: user_pw });
-    
-    return res.status(201).send({ msg:"회원가입을 축하합니다.",success: true });      
-    })
-);
+router.post('/', checkRegister, postRegister);
 
 module.exports = router;
